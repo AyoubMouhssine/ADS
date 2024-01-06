2f60501b8ef2679ee9b297f9b84d998f3dc4dac4
Ayoub <ayoub.mouhssine00@gmail.com>
2024-01-06 20:05:05 +0100

    adding bubble sort algorithm

--- sort/bubble_sort.js
+++ sort/bubble_sort.js
@@ -1,3 +1,13 @@
-function bubble_sort() {
-    
+function bubble_sort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array
 }
